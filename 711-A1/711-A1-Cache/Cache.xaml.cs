76b5faebd840270340f53515f31c2ec04e11f2e4
J C <jarak_jakar@hotmail.com>
2016-07-26 17:54:49 +1200

    Seemingly got the getfiles method for accessing the list of files on the server going.  Sort of.  The listview in the cache program does kinda update with the list of files available in the server folder at least.

---
+++ 711-A1/711-A1-Cache/Cache.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using _711_A1_Cache.ServiceReference1;
+
+namespace _711_A1
+{
+    /// <summary>
+    /// Interaction logic for CacheApp.xaml
+    /// </summary>
+    public partial class CacheApp : Application
+    {
+        //ServerServiceClient client = new ServerServiceClient();
+    }
+}
---
+++ 711-A1/711-A1-Cache/CacheWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _711_A1_Cache.ServiceReference1;
+
+namespace _711_A1
+{
+    /// <summary>
+    /// Interaction logic for CacheWindow.xaml
+    /// </summary>
+    public partial class CacheWindow : Window
+    {
+        ServerServiceClient client;
+
+        public CacheWindow()
+        {
+            InitializeComponent();
+            client = new ServerServiceClient();
+        }
+
+        private void listFilesButton_Click(object sender, RoutedEventArgs e)
+        {
+            string[] serverFileList = client.GetFileList();
+            filesListView.ItemsSource = serverFileList;
+            
+        }
+    }
+}
